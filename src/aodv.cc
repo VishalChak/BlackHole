77e2067a9552ca48979380b6a43df701648adec8
VishalChak <vishalbabu.in@gmail.com>
2016-01-24 14:21:45 +0530

    t

--- src/aodv.cc
+++ src/aodv.cc
@@ -7,4 +7,4 @@ Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
 
 
-1. Redistributions of source code must retain the above copyright no
+1. Redistributions of source code must retain the above copyright not
