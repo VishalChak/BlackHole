d338d3d173f5e73db7054efe9ede7cac0ee0892b
VishalChak <vishalbabu.in@gmail.com>
2016-01-25 10:46:12 +0530

    O

--- src/aodv.cc
+++ src/aodv.cc
@@ -17,4 +17,4 @@ derived from this software without specific prior written permission.
 
 
 THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
-IMPLIED WARRANTIES, INCLUDING, BUT N
+IMPLIED WARRANTIES, INCLUDING, BUT NO
